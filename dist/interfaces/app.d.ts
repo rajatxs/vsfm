a14a79a30fafc38408b1f304d0f6b31a75f00a52
Rajat Sharma <rxx175@gmail.com>
2022-03-13 18:38:29 +0530

    Removed dist

--- dist/interfaces/app.d.ts
+++
@@ -1,3 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-export declare type AppNextFunction = () => void;
-export declare type AppMiddleware = (req: VercelRequest, res: VercelResponse, next?: AppNextFunction) => any;
--- dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-import { AppMiddleware, AppNextFunction } from './interfaces/app';
-export declare function compose(...middlewares: AppMiddleware[]): AppMiddleware;
-export { AppMiddleware, AppNextFunction };
